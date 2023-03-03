3032a935e605492cc59f060de79761c54f332e44
technicalguftgu <sachintekode1994@gmail.com>
2023-03-03 15:19:17 +0530

    adding new project to the bit-bucket

---
+++ javatheme/src/main/java/newjavaproject/App.java
@@ -0,0 +1,13 @@
+package newjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
